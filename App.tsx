f51d34ca1b6f8b6f0aad27ead0bf3090eebc3d88
VictoriaFontalvo <daniel.farina@correo.unimet.edu.ve>
2025-05-25 18:00:59 -0400

    Historial de mantenimiento inicial

--- App.tsx
+++ App.tsx
@@ -1,15 +1,5 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
 import MaintenanceLog from './components/MaintenanceLog';
-import './global.css';
-
 
 export default function App() {
-  return (
-    <>
-      <MaintenanceLog />
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
+  return <MaintenanceLog />;
 }
