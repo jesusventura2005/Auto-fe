3548ebe3aa920c88cb66f23545fa2a6ec4cb6791
VictoriaFontalvo <daniel.farina@correo.unimet.edu.ve>
2025-05-25 17:50:20 -0400

    Resolver conflictos de pull de develop

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import MaintenanceLog from './components/MaintenanceLog';
 import './global.css';
+
 
 export default function App() {
   return (
     <>
-    
+      <MaintenanceLog />
       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
