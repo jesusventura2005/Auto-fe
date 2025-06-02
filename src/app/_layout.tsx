7c9a47665c38f3d0d1d7e9b177e280c20e18f481
Elias <manchai.fung@gmail.com>
2025-06-01 23:22:59 -0400

    feat!: created auth

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,16 +2,18 @@ import { Slot } from 'expo-router';
 import { View } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../../global.css';
+import { AuthProvider } from './context/AuthContext';
 
 export default function Layout() {
-
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
-    <View className="flex-1 justify-center bg-slate-600">
-      <QueryClientProvider client={queryClient}>
-        <Slot></Slot>
-      </QueryClientProvider>
-    </View>
+    <AuthProvider>
+      <View className="flex-1 justify-center bg-slate-600">
+        <QueryClientProvider client={queryClient}>
+          <Slot></Slot>
+        </QueryClientProvider>
+      </View>
+    </AuthProvider>
   );
 }
