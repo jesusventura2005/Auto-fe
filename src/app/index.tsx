051a8a3a13b6ea0c9f79c553eb2478b56bcc68be
Jesus Ventura <jesusventura2005@gmail.com>
2025-06-10 10:51:57 -0400

    feat: add tab bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,47 +1,45 @@
 import { View, ScrollView, Text } from 'react-native';
-import { Link, useRouter } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { useAuth } from '../context/AuthContext';
 import Header from '~/components/Header';
 import { Card } from '~/components/Card';
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
 const Index = () => {
-  // const { authState } = useAuth();
-  // const router = useRouter();
+  const { authState } = useAuth();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (authState?.authenticated) {
-  //     router.replace('/(home)/(maintenance)/maintenanceLog');
-  //   }
-  // }, [authState?.authenticated, router]);
+  useEffect(() => {
+    if (authState?.authenticated) {
+      router.replace('/(home)/(maintenance)/maintenanceLog');
+    }
+  }, [authState?.authenticated, router]);
 
-  // if (authState?.authenticated) {
-  //   return null;
-  // }
+  if (authState?.authenticated) {
+    return null;
+  }
 
-  return (
-      <ScrollView className='m-4 pt-12'>
-        <Header />
-        <View className='mt-12 flex justify-center items-center'>
-          <Text className='font-bold color-white text-4xl'>
-            Manten tu vehiculo en
-          </Text>
-          <Text className='font-bold color-[#005ee2] text-4xl'>
-            perfecto estado
-          </Text>
+  return ( 
+    <ScrollView className='p-3 pt-12 bg-[#111827]'>
 
-          <Text className='mt-5 font-light text-center text-2xl color-white'>
+      <Header />
+      <View className='mt-12 flex justify-center items-center'>
+        <Text className='font-bold color-white text-4xl'>
+          Manten tu vehiculo en
+        </Text>
+        <Text className='font-bold color-[#005ee2] text-4xl'>
+          perfecto estado
+        </Text>
+
+        <Text className='mt-5 font-light text-center text-2xl color-white'>
           La aplicación más completa para el seguimiento del mantenimiento de tu vehículo. Nunca olvides una revisión importante y mantén tu auto funcionando como nuevo.
-          </Text>
-        </View>
+        </Text>
+      </View>
 
-        <Card>
-          
-        </Card>
-
-      </ScrollView>
-
-
+      <Card>
+      </Card>
+    </ScrollView>
+   
   );
 };
 
