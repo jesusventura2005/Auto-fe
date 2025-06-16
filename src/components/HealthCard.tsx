0fc7d4f2305ddc82b3d7760e40249a7af539266c
Elias <manchai.fung@gmail.com>
2025-06-16 02:04:12 -0400

    minor fix

--- src/components/HealthCard.tsx
+++ src/components/HealthCard.tsx
@@ -6,10 +6,10 @@ export default function HealthCard() {
     <View className="bg-red-400shadow-md flex w-11/12 flex-col overflow-hidden rounded-xl border-2 border-gray-200">
       <View className="flex w-full flex-row items-center gap-2 bg-[#005ee2]">
         <Ionicons className="p-3" name="heart-outline" size={24} color="white" />
-        <Text className="text-xl text-white">Vehicle Health</Text>
+        <Text className="text-xl font-bold text-white">Vehicle Health</Text>
       </View>
       <View className="flex w-full flex-col items-start gap-3 p-4">
-        <Text className="text-2xl font-bold text-black">Health Card</Text>
+        <Text className="text-2xl font-bold text-black">Car Current Condition</Text>
         <Text className="text-base text-gray-600">
           This card provides an overview of the vehicles health status, including diagnostics and
           maintenance
