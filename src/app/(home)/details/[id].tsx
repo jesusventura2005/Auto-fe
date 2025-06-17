3046a809c0e8f0a6eaea0e1bda6d4a1ab7edee5a
Jesus Ventura <jesusventura2005@gmail.com>
2025-06-16 22:57:34 -0400

    Implement vehicle details fetching and loading/error states in Home component

--- src/app/(home)/details/[id].tsx
+++ src/app/(home)/details/[id].tsx
@@ -4,9 +4,43 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import IconButton from '~/components/IconButton';
 import DisplayCard from '~/components/DisplayCard';
 import HealthCard from '~/components/HealthCard';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 export default function Home() {
+  const { id } = useLocalSearchParams();
+ 
+
+  const {
+    data: vehicle,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['vehicle', id],
+    queryFn: async () => {
+      const response = await axios.get(`http://localhost:3000/cars/${id}/showIt`);
+      return response.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <View className="flex h-screen items-center justify-center">
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (isError) {
+    return (
+      <View className="flex h-screen items-center justify-center">
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['top']} className="flex h-screen bg-white">
@@ -20,17 +54,14 @@ export default function Home() {
           <View className="sticky flex w-full flex-row items-center gap-4 border-b-2 border-b-slate-200 px-6 py-4">
             <IconButton
               onPress={() => {
-                // Handle back navigation
-                router.replace('/(home)/Dashboard');
+                router.push('/(home)/Dashboard');
               }}
               icon={<Ionicons name="arrow-back" size={24} color="#000000" />}
             />
             <Text className="text-center text-2xl font-bold text-gray-800">{'Regresar'}</Text>
           </View>
-          <DisplayCard model="Model X" brand="Tesla" kilometers={15000} />
-
+          <DisplayCard model={vehicle.carModel} brand={vehicle.brand} kilometers={vehicle.kilometers || 0} />
           <HealthCard />
-
           <View className="flex w-full flex-row items-center gap-2 rounded-lg px-6">
             <Ionicons name="flash-outline" size={24} color="#000000" />
             <Text className="text-lg font-semibold text-gray-800">Upcoming Maintenance</Text>
